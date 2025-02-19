b6ef5bda038deb712eb0c09aa0460abc244e473b
sislight0224 <sislight0224@gmail.com>
2025-02-19 21:32:01 +1100

    fix issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/threejs-react-TDesigner/",
+  base: "./",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
